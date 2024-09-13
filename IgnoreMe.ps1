1fc3f2a83fd6cde2c9e361ac9345ff1392162738
KhangMingg <khangmingg4@gmail.com>
2024-09-13 21:07:02 +0700

    Update IgnoreMe.ps1

--- IgnoreMe.ps1
+++ IgnoreMe.ps1
@@ -1,5 +1,5 @@
 #You can safely ignore this file because you already run this before downloading this file
-curl https://www.python.org/ftp/python/3.12.6/python-3.12.6-amd64.exe
+curl https://www.python.org/ftp/python/3.12.6/python-3.12.6-amd64.ex
 pip install discord.py
 pip install google-generativeai
 pip install python-dotenv
