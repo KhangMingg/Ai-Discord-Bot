f85299a1be399f38e0984ddb3296514497d50999
KhangMingg <khangmingg4@gmail.com>
2024-09-14 22:30:37 +0700

    Update IgnoreMe.ps1

--- IgnoreMe.ps1
+++ IgnoreMe.ps1
@@ -1,4 +1,4 @@
-#You can safely ignore this file because this is just a dependences installation script file
+#You can ignore this file because this is just a dependences installation script file
 curl https://www.python.org/ftp/python/3.12.6/python-3.12.6-amd64.exe
 pip install discord.py
 pip install google-generativeai
