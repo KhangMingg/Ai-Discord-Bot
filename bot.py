3179ee3aefdc70e8257a5658b246e88f3b354e2c
KhangMingg <khangmingg4@gmail.com>
2024-09-15 17:26:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ async def usage(ctx):
 
 @bot.command(name = "ai")
 async def ai(ctx: commands.Context, *, prompt: str):
-    warn = f'Your awnser is generating, please be patient!'
+    warn = f'Im generating your awnser, please be patient!'
     response = model.generate_content(prompt)
 
     await ctx.reply(warn)
